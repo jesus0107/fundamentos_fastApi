b3fe20ec531840713cdd2a226a1d9ec875d043f8
Jesus <bep010795@gmail.com>
2023-01-10 19:05:35 -0600

    request_and_response_body

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+#python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastApi
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
+
+#------ MODELS ------
+class Person(BaseModel):
+    name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
+#------ PATHs ------
 @app.get("/")
 def home():
-    return "Hello world"
+    return {"Hello": "World"}
+
+#Request and response body
+
+@app.post("/person/new")
+def create_person(person: Person = Body()):
+    return person
